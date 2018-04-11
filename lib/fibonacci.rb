4dbbd613d50f2ad28518c953b319a4615f463a6c
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-04-11 15:49:27 +0100

    tested green for new method add_to_existig_arr and retrieve_fibonacci_from_memory

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,35 +7,22 @@ class Fibonacci
 
 
   def fibo(n)
-
-
-
     arr =[]
-    if @existingArr == []
 
+    if @existingArr.empty?
       @existingArr = create_new_array(n)
 
     elsif @existingArr.length >= n
-      p "inside second if"
-      return @existingArr.join(" ,") if @existingArr.length == n
-      return @existingArr[0..n-1].join(" ,") if @existingArr.length > n
+
+      @existingArr = retrieve_fibonacci_from_memory(n, @existingArr)
 
     elsif @existingArr.length < n
       p 'Printing from memory'
-      length = @existingArr.length
-      arr = @existingArr[(length - 2)..(length - 1)]
 
-      itterateNumber = n - length
-
-      for i in 3..(itterateNumber + 2)
-        arr << (arr[i - 3] + arr[i - 2])
-      end
-
-      @existingArr = @existingArr[0..length - 3] + arr
-
+      @existingArr = add_to_existing_fibonacci_and_return_arr(n, @existingArr)
     end
 
-    return @existingArr.join(" ,")
+      @existingArr.join(" ,")
   end
 
   def create_new_array(n)
@@ -45,5 +32,23 @@ class Fibonacci
       arr << (arr[i - 3] + arr[i - 2]) if i > 2
     end
     arr
+  end
+
+  def retrieve_fibonacci_from_memory(n, existingArr)
+    return existingArr if existingArr.length == n
+    return existingArr[0..n-1] if existingArr.length > n
+  end
+
+  def add_to_existing_fibonacci_and_return_arr(n, existingArr)
+    length = existingArr.length
+    arr = existingArr[(length - 2)..(length - 1)]
+
+    itterateNumber = n - length
+
+    for i in 3..(itterateNumber + 2)
+      arr << (arr[i - 3] + arr[i - 2])
+    end
+
+    existingArr = existingArr[0..length - 3] + arr
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -12,11 +12,29 @@ describe  Fibonacci do
       fibonacci.fibo(4)
       expect(fibonacci.fibo(4)).to eq("1 ,1 ,2 ,3")
     end
+
+    it "Should add to existing fibonacci and return" do
+      expect(fibonacci.fibo(5)).to eq("1 ,1 ,2 ,3 ,5")
+    end
   end
 
   describe '#create_new_array' do
     it "Should create new array" do
       expect(fibonacci.create_new_array(4)).to eq([1 ,1 ,2 ,3])
+    end
+  end
+
+  describe '#retrieve_fibonacci_from_memory' do
+    it "Return fibonacci from memory and not compute" do
+      existingArr = [1 ,1 ,2 ,3]
+      expect(fibonacci.retrieve_fibonacci_from_memory(4, existingArr)).to eq([1 ,1 ,2 ,3])
+    end
+  end
+
+  describe '#add_to_existing_fibonacci_and_return' do
+    it "Add to existing fibonacci and return" do
+      existingArr = [1 ,1 ,2 ,3]
+      expect(fibonacci.add_to_existing_fibonacci_and_return_arr(5, existingArr)).to eq([1 ,1 ,2 ,3, 5])
     end
   end
 end
