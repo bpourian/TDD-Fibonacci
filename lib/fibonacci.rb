9f22b1863a75ff101975f2d0abe2bf7c5c8b4c0c
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-04-11 12:19:57 +0100

    added logic for memorisation

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,19 +1,43 @@
 class Fibonacci
+  attr_reader :existingArr
+
+  def initialize
+    @existingArr = []
+  end
+
 
   def fibo(n)
-    existingArr = []
     arr =[]
 
-    if existingArr.length >= n
-      return existingArr if existingArr.length == n
-      return existingArr[0..n-1] if existingArr.length > n
-    else
+    if @existingArr == []
+      'inside first if'
       for i in 1..n
         arr << 1 if i == 1 || i == 2
         arr << (arr[i - 3] + arr[i - 2]) if i > 2
       end
+
+      @existingArr = arr
+
+    elsif @existingArr.length >= n
+      "inside second if"
+      return @existingArr.join(" ,") if @existingArr.length == n
+      return @existingArr[0..n-1].join(" ,") if @existingArr.length > n
+
+    elsif @existingArr.length < n
+      'inside third else'
+      length = @existingArr.length
+      arr = @existingArr[(length - 2)..(length - 1)]
+
+      itterateNumber = n - length
+
+      for i in 3..(itterateNumber + 2)
+        arr << (arr[i - 3] + arr[i - 2])
+      end
+
+      @existingArr = @existingArr[0..length - 3] + arr
+
     end
-    existingArr = arr
-    return existingArr.join(" ,")
+
+    return @existingArr.join(" ,")
   end
 end
