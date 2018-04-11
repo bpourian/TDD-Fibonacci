24015cc3bd28a3f31fd493dbb9c913167a18f927
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-04-11 10:00:01 +0100

    testing from memory

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,11 +1,19 @@
 class Fibonacci
 
   def fibo(n)
+    existingArr = []
     arr =[]
-    for i in 1..n
-      arr << 1 if i == 1 || i == 2
-      arr << (arr[i - 3] + arr[i - 2]) if i > 2
+
+    if existingArr.length >= n
+      return existingArr if existingArr.length == n
+      return existingArr[0..n-1] if existingArr.length > n
+    else
+      for i in 1..n
+        arr << 1 if i == 1 || i == 2
+        arr << (arr[i - 3] + arr[i - 2]) if i > 2
+      end
     end
-    return arr.join(" ,")
+    existingArr = arr
+    return existingArr.join(" ,")
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -7,5 +7,9 @@ describe  Fibonacci do
     it "Should return all the fibonacci numbers up to n = 4" do
       expect(fibonacci.fibo(4)).to eq("1 ,1 ,2 ,3")
     end
+
+    it "Should return fibonacci from memory and not compute" do
+      expect(fibonacci.fibo(4)).to eq("1 ,1 ,2 ,3")
+    end
   end
 end
