da32e20bd291d33ab397249c3a59b9b486ae8581
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-04-11 14:56:44 +0100

    created new method called 'create_new_array' and tested green

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,24 +7,21 @@ class Fibonacci
 
 
   def fibo(n)
+
+
+
     arr =[]
-
     if @existingArr == []
-      'inside first if'
-      for i in 1..n
-        arr << 1 if i == 1 || i == 2
-        arr << (arr[i - 3] + arr[i - 2]) if i > 2
-      end
 
-      @existingArr = arr
+      @existingArr = create_new_array(n)
 
     elsif @existingArr.length >= n
-      "inside second if"
+      p "inside second if"
       return @existingArr.join(" ,") if @existingArr.length == n
       return @existingArr[0..n-1].join(" ,") if @existingArr.length > n
 
     elsif @existingArr.length < n
-      'inside third else'
+      p 'Printing from memory'
       length = @existingArr.length
       arr = @existingArr[(length - 2)..(length - 1)]
 
@@ -39,5 +36,14 @@ class Fibonacci
     end
 
     return @existingArr.join(" ,")
+  end
+
+  def create_new_array(n)
+    arr =[]
+    for i in 1..n
+      arr << 1 if i == 1 || i == 2
+      arr << (arr[i - 3] + arr[i - 2]) if i > 2
+    end
+    arr
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -9,7 +9,14 @@ describe  Fibonacci do
     end
 
     it "Should return fibonacci from memory and not compute" do
+      fibonacci.fibo(4)
       expect(fibonacci.fibo(4)).to eq("1 ,1 ,2 ,3")
+    end
+  end
+
+  describe '#create_new_array' do
+    it "Should create new array" do
+      expect(fibonacci.create_new_array(4)).to eq([1 ,1 ,2 ,3])
     end
   end
 end
